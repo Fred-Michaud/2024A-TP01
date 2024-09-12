1cba278e6b3b41fa2e06c2ed228f3da078a860a1
ydna985 <andy-abdoul-mouhaimine.koutou@polymtl.ca>
2024-09-12 19:21:28 -0400

    number 5

--- ex5.py
+++ ex5.py
@@ -1,5 +1,9 @@
 #TODO: Analyser la chaîne de caractères saisie et compter le nombre de médailles.
 #      Attention si la chaîne est invalide, un message d'erreur est attendu.
 
-country = ...
-code_medals = ...
+country = input("Pays conserné? ")
+code_medals = input("Chaine représentant les médailles? ").upper()
+print(f"""{country}:
+-{code_medals.count('G')} Or
+-{code_medals.count('S')} Argent
+-{code_medals.count('B')} Bronze""")
