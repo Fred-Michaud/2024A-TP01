36a7abaec5808af3050589a1eca1bfda7f7a118b
Frédéric Michaud <fredmichaud@Frederics-MacBook-Pro-2.local>
2024-09-11 20:22:23 -0400

    Fait ex1

--- ex1.py
+++ ex1.py
@@ -1,7 +1,7 @@
 # TODO: Créer un script permettant le formattage du livre des records des JO.
 
 
-#différentes questions a répondre
+#relever le nouveau record
 country = input("De quelle nationalité est l'athlète ? ")
 athlete = input("Quel est son nom?")
 date = input("Quelle est la date du record?")
@@ -9,8 +9,7 @@ sport = input("Quelle est la discipline?")
 category = input("Quelle est la catégorie?")
 record = input("Quel est le record?")
 
-#record
-
+#résumé du nouveau record
 txt = f"Nouveau record \n ------------- \n Pays: {country} \
 Discipline: {sport} \n Catégorie: {category} \n Date: {date} \
 \n Nom: {athlete} \n Record: {record}"
