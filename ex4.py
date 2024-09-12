67fd22306df5ded381de98b683ecd955b1cd31ac
ydna985 <andy-abdoul-mouhaimine.koutou@polymtl.ca>
2024-09-12 19:54:56 -0400

    number 4 change

--- ex4.py
+++ ex4.py
@@ -5,27 +5,27 @@
 
 battery_level = int(input("Quel est le pourcentage de batterie du bateau?: "))
 number_km_per_litter=0
-substitute=battery_level
+battery_level_left=battery_level
 number_km_possible=0
 
-while substitute !=0:
-    if substitute>=50 and substitute <= 100:
+while battery_level_left !=0:
+    if battery_level_left>50 and battery_level_left <= 100:
         number_km_per_litter=2
-    elif substitute>=25 and substitute <= 50:
+    elif battery_level_left>25 and battery_level_left <= 50:
         number_km_per_litter=0.5
-    elif substitute>=10 and substitute <= 25:
+    elif battery_level_left>10 and battery_level_left <= 25:
         number_km_per_litter=1
-    elif substitute>=5 and substitute <= 10:
+    elif battery_level_left>5 and battery_level_left <= 10:
         number_km_per_litter=2.5
-    elif substitute>=0 and substitute <= 5:
+    elif battery_level_left>0 and battery_level_left <= 5:
         number_km_per_litter=6
     
-    if(substitute >=1 ):
+    if(battery_level_left >=1 ):
         number_km_possible+=number_km_per_litter
-        substitute-=1
+        battery_level_left-=1
     else:
-     number_km_possible=number_km_possible+number_km_per_litter*substitute
-     substitute=0
+     number_km_possible=number_km_possible+number_km_per_litter*battery_level_left
+     battery_level_left=0
     
 
 print(f"La distance possible est de {round(number_km_possible, 1)} km")
