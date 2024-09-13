3c24fac5a720f0506c84e91f742eadb1fc48f44d
ydna985 <andy-abdoul-mouhaimine.koutou@polymtl.ca>
2024-09-12 20:33:12 -0400

    number one adjustment

--- ex1.py
+++ ex1.py
@@ -3,16 +3,15 @@
 
 #relever le nouveau record
 country = input("De quelle nationalité est l'athlète ? ")
-athlete = input("Quel est son nom?")
-date = input("Quelle est la date du record?")
-sport = input("Quelle est la discipline?")
-category = input("Quelle est la catégorie?")
-record = input("Quel est le record?")
+athlete = input("Quel est son nom? ")
+date = input("Quelle est la date du record? ")
+sport = input("Quelle est la discipline? ")
+category = input("Quelle est la catégorie? ")
+record = input("Quel est le record? ")
 
 #résumé du nouveau record
-txt = f"Nouveau record \n ------------- \n Pays: {country} \
-Discipline: {sport} \n Catégorie: {category} \n Date: {date} \
-\n Nom: {athlete} \n Record: {record}"
+txt = f"\nNouveau record:\n--------------------\n{date} - {sport} - {category}:\n\
+\t{athlete} ({country}) - {record}"
 
 print(txt)
 
