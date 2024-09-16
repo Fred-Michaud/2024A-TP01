e8dede986dc7deaeb525d55d8d0f760db28ce64e
Frédéric Michaud <fredmichaud@Frederics-MacBook-Pro-2.local>
2024-09-16 17:13:28 -0400

    Complétion de l'exercice 3

--- ex3.py
+++ ex3.py
@@ -1,5 +1,24 @@
 # TODO: Calculer la distance qu'un poids peut atteindre en utilisant la formule de la portée d'un projectile.
 # TODO: Importer les modules nécessaires.
+import math
 
-speed = ... 
-angle = ...
+#Caractéristiques uniques des lancers
+speed = float(input("Vitesse initiale (m/s)"))
+angle = float(input("Angle de lancement (degrés)"))
+
+#Constantes
+height = 2
+g = 9.81
+
+#Composantes horizontale et verticale de la vitesse
+speed_x = speed * math.cos(math.radians(angle))
+speed_y = speed * math.sin(math.radians(angle))
+
+#Utilisation d'une formule qui tient compte de la hauteur initiale de 2m
+distance = ((speed_x / g)*(speed_y+ math.sqrt(speed_y**2+2*g*height)))
+
+distance_arrondie = round(distance, 2)
+
+print ( f" La distance parcourue est : {distance_arrondie} mètres")
+
+
